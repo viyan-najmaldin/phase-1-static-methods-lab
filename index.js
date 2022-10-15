f3557a7c99d515567fc96ec14bd4b5289c24b51e
viyan-najmaldin <najmadinviyan@gmail.com>
2022-10-16 00:37:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, "");
+  }
+
+  static titleize(str) {
+    if (!str) {
+      return "";
+    }
+    let arr = [];
+    let exludeWords = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
+    arr = str.split(" ");
+    return arr
+      .map((word, i) => {
+        return exludeWords.includes(word) && i != 0
+          ? [word]
+          : word.charAt(0).toUpperCase() + word.slice(1);
+      })
+      .join(" ");
+  }
 }
